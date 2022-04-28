a2605dfab95caafd5eb3b4c412699d3b35db4242
Luis Alfredo Figueroa Bracamontes <92luisalfredo@protonmail.com>
2022-04-27 22:19:04 -0500

    Empezando maquetación nueva

---
+++ Prueba HDMI/Generador video.vhd
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity GeneradorVideo is
+    port (
+        CLK : in std_logic;
+        
+    );
+end entity GeneradorVideo;
