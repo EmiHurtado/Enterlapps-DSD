a664c0718fc0b1616ba3e00c8a21a2fb452788c6
AxelAdrian2002 <60492125+AxelAdrian2002@users.noreply.github.com>
2022-05-18 11:23:31 -0500

    Practica 4 terminada

--- Practica 4/Otro/contador8bits.vhd
+++ Practica 4/Otro/contador8bits.vhd
@@ -1,52 +1,50 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.std_logic_unsigned.ALL;
 
-entity Practica4 is
+ENTITY Practica4 IS
 
-	port(  clk50Mhz : in std_logic;
-			 leds : out std_logic_vector (7 downto 0)); 
-			 end Practica4;
-	
-architecture cuentabinario of Practica4 is
-	
-signal numeros: std_logic_vector (7 downto 0) :="00000000"; --se inicializa el vector en ceros.
-	 
-signal reloj_divisor: std_logic;
-	 
-	 
-component Reloj is    	 --  el archivo divisor de frecuencia debe estar en la misma carpeta! 
-	                      -- aqui se va a incluir el programa divisor de frecuencia que ya se implementó para trabajar 
-								 -- con una frecuencía mas baja en este caso 2Hz!!
+	PORT (
+		clk50Mhz, RST : IN STD_LOGIC;
+		leds : OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
+END Practica4;
 
-	 port( 
-	    salida: out std_logic;   -- se copia directamente de los puertos de entrada y salida, del programa reloj!
-       clk:    in std_logic;
-		 reset:  in std_logic );  
-		 
-end component ;
-	 
-	 
-	 
-begin 
-	 
-	Reloj1: Reloj port map( reloj_divisor, Clk50Mhz, '1') ;	 --  aqui se relaciona las entradas y salidas de los dos programas!
-	 
-	proceso_contador: process(reloj_divisor)
-	 
-	begin
-	 
-		if  rising_edge(reloj_divisor) 
-		then
-	 
-			numeros<= numeros +1;
-	 
-		end if;
-	 
-	end process;
-	
-   leds<= numeros;  -- checar si son de ánodo común o de cátado común!
-	
-end cuentabinario;
-	
-	
+ARCHITECTURE cuentabinario OF Practica4 IS
+
+	SIGNAL numeros : STD_LOGIC_VECTOR (7 DOWNTO 0) := "00000000"; --se inicializa el vector en ceros.
+
+	SIGNAL reloj_divisor : STD_LOGIC;
+	COMPONENT Reloj IS --  el archivo divisor de frecuencia debe estar en la misma carpeta! 
+		-- aqui se va a incluir el programa divisor de frecuencia que ya se implementó para trabajar 
+		-- con una frecuencía mas baja en este caso 2Hz!!
+
+		PORT (
+			salida : OUT STD_LOGIC; -- se copia directamente de los puertos de entrada y salida, del programa reloj!
+			clk : IN STD_LOGIC;
+			reset : IN STD_LOGIC);
+
+	END COMPONENT;
+
+BEGIN
+
+	Reloj1 : Reloj PORT MAP(reloj_divisor, Clk50Mhz, '1'); --  aqui se relaciona las entradas y salidas de los dos programas!
+
+	proceso_contador : PROCESS (reloj_divisor, RST)
+
+	BEGIN
+
+		IF (RST = '0')
+			THEN
+			numeros <= "00000000";
+		ELSIF rising_edge(reloj_divisor)
+			THEN
+
+			numeros <= numeros + 1;
+
+		END IF;
+
+	END PROCESS;
+
+	leds <= numeros; -- checar si son de ánodo común o de cátado común!
+
+END cuentabinario;
